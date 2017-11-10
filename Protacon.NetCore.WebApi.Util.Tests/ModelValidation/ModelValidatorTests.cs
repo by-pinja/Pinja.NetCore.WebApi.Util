b54e9c58215268157eef18c5815d86b3dec91174
Savpek <savolainen.pekka@gmail.com>
2017-11-10 08:58:27 +0200

    Added model validator.

---
+++ Protacon.NetCore.WebApi.Util.Tests/ModelValidation/ModelValidatorTests.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using Protacon.NetCore.WebApi.Util.ModelValidation;
+using Xunit;
+
+namespace Protacon.NetCore.WebApi.Util.Tests.ModelValidation
+{
+    public class ModelValidatorTests
+    {
+        public class ValidModel
+        {
+            public string NotChecked { get; set; }
+
+            [Required]
+            public string Required { get; set; } = "ABC VALUE HERE";
+
+        }
+
+        public class InvalidModel
+        {
+            [Required]
+            public string RequiredExampleField { get; set; }
+        }
+
+        [Fact]
+        public void WhenModelIsInvalid_ThenItErrorShouldBeReturned()
+        {
+            ModelValidator.TryFindErrorsFromModel(new InvalidModel(), out var error).Should().Be(true);
+            error.Message.Should().Be("RequiredExampleField: The RequiredExampleField field is required.");
+            error.Code.Should().Be("INVALID_MODEL");
+        }
+
+        [Fact]
+        public void WhenValidModelIsUsed_ThenReturnFalseAndNoErrors()
+        {
+            ModelValidator.TryFindErrorsFromModel(new ValidModel(), out var error).Should().Be(false);
+            error.Should().Be(null);
+        }
+    }
+}
---
+++ Protacon.NetCore.WebApi.Util/ModelValidation/ModelValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Protacon.NetCore.WebApi.Util.ModelValidation
+{
+    public static class ModelValidator
+    {
+        public static bool TryFindErrorsFromModel(object model, out ApiError error)
+        {
+            var vc = new ValidationContext(model);
+            var errors = new List<ValidationResult>();
+
+            Validator.TryValidateObject(model, vc, errors);
+
+            if (errors.Count == 0)
+            {
+                error = null;
+                return false;
+            }
+
+            error = new ApiError("INVALID_MODEL", ParseMessage(errors));
+            return true;
+        }
+
+        private static string ParseMessage(List<ValidationResult> validationResult)
+        {
+            return validationResult
+                .Aggregate("", (a, b) => $"{a}, {ParseMemberNames(b)}: {b}").Trim(',').Trim();
+        }
+
+        private static object ParseMemberNames(ValidationResult validationResult)
+        {
+            return validationResult.MemberNames
+                .Aggregate("", (a, b) => $"{a}, {b}").Trim(',').Trim();
+        }
+    }
+}
